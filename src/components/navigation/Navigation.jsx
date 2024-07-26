e61094a097d454534b62f708d321f6c7114aa53f
Jaylen Garner <jaylen@moonraydevelopment.com>
2024-07-26 19:07:16 -0400

    Additional styling

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -4,7 +4,8 @@ import React from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { navigationAnimationVariants } from "./helpers";
-import { pages } from "@/components/navigation/helpers";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Navigation = () => {
   return (
@@ -14,18 +15,32 @@ const Navigation = () => {
       initial="initial"
       animate="animate"
     >
-      <Link href={"/"} className="text-4xl italic text-[#20603D]">
+      <Link href={"/"} className="text-4xl italic text-[#20603D] link_hover">
         P.K.
       </Link>
       <div className="space-x-6 text-slate-900 text-lg">
-        {pages.map((page) => {
-          return (
-            <Link href={`${page.link}`} key={page.name}>
-              {page.name}
-            </Link>
-          );
-        })}
+        <Link href={`/`} className="link_hover">
+          About
+        </Link>
+        <Link
+          href={`https://priyana-kale.s3.amazonaws.com/Public/Assets/resume.pdf`}
+          target="_blank"
+          className="link_hover"
+        >
+          Resume
+        </Link>
+        <Link href={`/contact-me`} className="link_hover">
+          Contact
+        </Link>
       </div>
+      <Link href={"https://www.linkedin.com/in/priyana-kale/"} target="_blank">
+        <FontAwesomeIcon
+          icon={faLinkedin}
+          size="2xl"
+          color="#0966C2"
+          className="link_hover"
+        />
+      </Link>
     </motion.nav>
   );
 };
--- src/components/navigation/helpers.js
+++ src/components/navigation/helpers.js
@@ -1,16 +1,6 @@
-const pages = [
-  { name: "About", link: "/" },
-  { name: "Work", link: "/work" },
-  {
-    name: "Resume",
-    link: "https://priyana-kale.s3.amazonaws.com/Public/Assets/resume.pdf",
-  },
-  { name: "Contact", link: "/contact-me" },
-];
-
 const navigationAnimationVariants = {
   initial: { y: -20, opacity: 0 },
   animate: { y: 0, opacity: 1, transition: { delay: 0.2, duration: 0.6 } },
 };
 
-module.exports = { pages, navigationAnimationVariants };
+module.exports = { navigationAnimationVariants };
